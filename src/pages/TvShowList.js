b4b088f9aae52939155165b33bb999a85fb42c72
leemihyun <m.lee@cosmocomputing.co.jp>
2023-08-05 22:06:19 +0900

    add: reactquery 적용(tvShows)

--- src/pages/TvShowList.js
+++ src/pages/TvShowList.js
@@ -1,36 +1,38 @@
 import React, {useEffect, useState} from 'react';
 import axios, {get} from "axios";
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
+import {usePaginatedFetchTvShows} from "../services/PaginateTvShows";
 
 const TvShowList = () => {
-    const [tvShows, setTvShows] = useState([]);
-    const getTvShowList = async () => {
-        try {
-            const url = 'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1';
-            const options = {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmI5MGJjZGRkM2U4YTVmN2ExNWUxMTI1ZjM2MWNhYyIsInN1YiI6IjY0YzUxODYyOWI2ZTQ3MDBmZjM2NDc3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jSXr5VU0QSguF3cxV42p2Z7EJmO9J-Y6vp9mJRW7Y7g'
-                }
-            };
+    // const [tvShows, setTvShows] = useState([]);
+    // const getTvShowList = async () => {
+    //     try {
+    //         const url = 'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1';
+    //         const options = {
+    //             method: 'GET',
+    //             headers: {
+    //                 accept: 'application/json',
+    //                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmI5MGJjZGRkM2U4YTVmN2ExNWUxMTI1ZjM2MWNhYyIsInN1YiI6IjY0YzUxODYyOWI2ZTQ3MDBmZjM2NDc3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jSXr5VU0QSguF3cxV42p2Z7EJmO9J-Y6vp9mJRW7Y7g'
+    //             }
+    //         };
+    //
+    //         const {data, status} = await axios.get(url, options);
+    //         console.log('data ? : ', data.results)
+    //         console.log('status ? : ', status)
+    //         if (status === 200) {
+    //             setTvShows(data.results)
+    //         }
+    //     } catch (err) {
+    //         console.log('error ! : ', err.message)
+    //     }
+    // }
+    // useEffect(() => {
+    //     getTvShowList();
+    // }, []);
 
-            const {data, status} = await axios.get(url, options);
-            console.log('data ? : ', data.results)
-            console.log('status ? : ', status)
-            if (status === 200) {
-                setTvShows(data.results)
-            }
-        } catch (err) {
-            console.log('error ! : ', err.message)
-        }
-    }
-    useEffect(() => {
-        getTvShowList();
-    }, []);
-
+    const {data: tvShows} = usePaginatedFetchTvShows(1)
     const imgUrl = `https://image.tmdb.org/t/p/w500`;
-
+    console.log('===> tvShows', tvShows)
     return (
         <Container>
             <h1 style={{ textAlign: "center", margin: "100px", fontSize: '68px'}}>TV shows List</h1>
---
+++ src/services/PaginateTvShows.js
@@ -0,0 +1,15 @@
+import tmdbApi from "./api";
+import {useQuery} from "@tanstack/react-query";
+
+const fetchPaginatedTvShows = async (page = 1) => {
+    const {data} = await tmdbApi.get(`/tv/top_rated?language=en-US&page=${page}`);
+    console.log("++++++++++++++", data.results)
+    return data.results;
+}
+
+const usePaginatedFetchTvShows = (page) =>
+    useQuery(["tvShow", page], () => fetchPaginatedTvShows(page), {
+        keepPreviousData: true,
+    })
+
+export { usePaginatedFetchTvShows};
