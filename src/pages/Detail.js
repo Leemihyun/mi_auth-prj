a7e2c5e8f23f5f26ba6ec1e77bdcd2a62b62e778
leemihyun <m.lee@cosmocomputing.co.jp>
2023-08-25 21:02:12 +0900

    add: detail update

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,7 +3,7 @@ import {useLocation, useNavigate, useParams} from "react-router-dom";
 import useFetchMovieAndTVById from "../services/FetchMovieAndTv";
 import axios from "axios";
 import api from "../services/api";
-import {Button, Container, Row, Spinner} from "react-bootstrap";
+import {Button, Card, Col, Container, Image, Row, Spinner, Table} from "react-bootstrap";
 
 
 const Detail = () => {
@@ -14,6 +14,11 @@ const Detail = () => {
     const id = location.pathname.includes('movie') ? movieId : tvId
     const provider = location.pathname.includes('movie') ? 'movie' : 'tv'
     const {data, isLoading} = useFetchMovieAndTVById(id, provider)
+
+    console.log('detail > data', data)
+    // console.log('genres', data?.genres )
+
+    const imgUrl = `https://image.tmdb.org/t/p/w500`;
 
     // const [data , setData] = useState({});
     // const getMovieOrTv = async () => {
@@ -49,8 +54,63 @@ const Detail = () => {
                     Go Back
                 </Button>
             </header>
-            <h1>{data?.title ? data.title : data.name}</h1>
-            <h3>{data?.overview}</h3>
+            <Row className="justify-content-md-center">
+                <Row className={'mt-5'}>
+                    <Card.Img style={{height: 'auto', width: '30%'}} variant="top" src={ imgUrl+data.poster_path
+                        ? imgUrl + data.poster_path : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkCMT1rKQSmpSiWrjn460FlW3dI2WThcBiwA&usqp=CAU"} />
+                    <Col>
+                        <h1>title: {data?.title ? data.title : data.name}</h1>
+                        <Table striped bordered hover variant="dark">
+                            <tbody>
+                            <tr>
+                                <td>language</td>
+                                <td>{data?.original_language}</td>
+                            </tr>
+                            <tr>
+                                <td>web site</td>
+                                <td>{data?.homepage}</td>
+                            </tr>
+                            <tr>
+                                {data?.genres && <td>genres</td> }
+                                <td style={{display: 'flex', direction: 'row'}}>
+                                    {data?.genres?.map((genre, i) =>{
+                                        return <p style={{marginRight: '5px'}}>{ genre.name }, </p>
+                                    })}
+                                </td>
+                            </tr>
+                            <tr>
+                                {data?.spoken_languages && <td>spoken_languages</td> }
+                                <td style={{display: 'flex', direction: 'row'}}>
+                                    {data?.spoken_languages?.map((lang, i) =>{
+                                        return <p style={{marginRight: '5px'}}>{ lang.english_name}</p>
+                                    })}
+                                </td>
+                            </tr>
+                            { movieId &&
+                                <tr>
+                                    <td>status</td>
+                                    <td>{data?.status}</td>
+                                </tr>
+                            }
+
+                            </tbody>
+                        </Table>
+                        <h4>[overview]</h4>
+                        <p>{data?.overview}</p>
+                        { movieId && data?.production_companies.map((com)=>{
+                            // eslint-disable-next-line array-callback-return
+                            return <Row>
+                            <Col className="flex-column align-items-center justify-content-center">
+                                <img style={{width: 'auto', maxHeight: '30px'}} src={`${imgUrl}${com.logo_path}`} alt={'img'} />
+                                {/*<hr />*/}
+                                <p style={{marginRight: '5px'}}> {com.name} / {com.origin_country}</p>
+                            </Col>
+                            </Row>
+                        })}
+                    </Col>
+                </Row>
+            </Row>
+
         </Container>
     );
 };
